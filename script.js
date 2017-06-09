63d1a878be1f5fcf0a2e13d023f1848ef139348d
Jose Ramirez <jsayram@Gmail.com>
2017-06-09 19:07:42 -0400

    Added javaScript code to minipulate the arms location

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// target the ids for each of the arms on the analog clock
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+
+//apply the numbers above as degrees
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
